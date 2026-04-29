77bd2472e3b09141b3e123265ac0ffd6a3956143
RhuanBoni <rhuanboni@newgesti.com.br>
2026-04-29 19:29:25 -0300

    feat(Color): color constants.

--- app/src/main/java/br/edu/ifsp/scl/sc3043983/fasttripplanner/ui/theme/Color.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3043983/fasttripplanner/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package br.edu.ifsp.scl.sc3043983.fasttripplanner.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val TravelPrimary = Color(0xFF1E88E5)
+val TravelSecondary = Color(0xFFFFB300)
+val TravelTertiary = Color(0xFF00ACC1)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val TravelPrimaryDark = Color(0xFF1565C0)
+val TravelSecondaryDark = Color(0xFFFF8F00)
+val TravelTertiaryDark = Color(0xFF00838F)
